3148c00ad39e619a48dadcb67f565813295512da
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-10 16:07:05 +0330

    Getting to know plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,72 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('Html-Webpack-Plugin');
+
 
 module.exports = {
-    entry : {
-        bundle : './src/script.js'
+    entry: {
+        bundle: './src/script.js'
     },
-    output : {
-        filename : '[name].js',
-        path : path.resolve(__dirname,'build'),
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'build'),
     },
-    module : {
-        rules : [
+    module: {
+        rules: [
             {
-                test : /\.css$/,
-                use : ['style-loader' , 'css-loader']
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
             },
             {
-                test : /\.s[ac]ss$/,
-                use : ['style-loader' , 'css-loader' , 'sass-loader']
+                test: /\.s[ac]ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             },
             {
-                test : /\.(png|jpe?g|gif)$/,
-                use : [
-                   {
-                       loader : 'file-loader',
-                       options : {
-                           publicPath : 'build/images',
-                           outputPath : 'images',
-                           name : '[name].[ext]'
-                       }
-                   } 
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            publicPath: 'build/images',
+                            outputPath: 'images',
+                            name: '[name].[ext]'
+                        }
+                    }
                 ]
             },
 
             {
-              test : /\.(woff|woff2|otf|ttf|eot)$/ ,
-              use : [
-                {
-                    loader : 'file-loader',
-                    options : {
-                        publicPath : 'build/fonts',
-                        outputPath : 'fonts',
-                        name : '[name].[ext]'
+                test: /\.(woff|woff2|otf|ttf|eot)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            publicPath: 'build/fonts',
+                            outputPath: 'fonts',
+                            name: '[name].[ext]'
+                        }
                     }
-                } 
-             ]
-          
+                ]
+
             },
 
             {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
-                  loader: "babel-loader",
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
-              },
+            },
         ]
-    }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title : 'hello everyOne',
+            template :'index.html'
+    })
+],
+
 }
